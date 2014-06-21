2ecc6442b6ed056c9c1d8ec409ff4b9af70a151a
jlfbuijssen <jlfbuijssen@gmail.com>
2014-06-21 15:10:41 +0200

    Massive layout change

--- ThermostatApp/gen/com/example/thermostatapp/R.java
+++ ThermostatApp/gen/com/example/thermostatapp/R.java
@@ -794,10 +794,11 @@ containing a value of this type.
         public static final int abc_split_action_bar_is_narrow=0x7f060002;
     }
     public static final class color {
-        public static final int abc_search_url_text_holo=0x7f070003;
+        public static final int abc_search_url_text_holo=0x7f070004;
         public static final int abc_search_url_text_normal=0x7f070000;
         public static final int abc_search_url_text_pressed=0x7f070002;
         public static final int abc_search_url_text_selected=0x7f070001;
+        public static final int dark_gray=0x7f070003;
     }
     public static final class dimen {
         /**  Default height of an action bar. 
@@ -973,6 +974,10 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
+        public static final int moon_icon=0x7f020058;
+        public static final int moon_icon_grey=0x7f020059;
+        public static final int sun_icon=0x7f02005a;
+        public static final int sun_icon_grey=0x7f02005b;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -989,8 +994,12 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int buttons_and_temperature=0x7f050040;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
+        public static final int date_layout=0x7f050047;
+        public static final int date_textview=0x7f050048;
+        public static final int day_textview=0x7f050049;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1002,12 +1011,19 @@ containing a value of this type.
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
+        public static final int icons_layout=0x7f05003c;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imageView_moon=0x7f05003e;
+        public static final int imageView_sun=0x7f05003d;
+        public static final int layout_additional_info=0x7f050046;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int live_layout=0x7f05003f;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
+        public static final int next_switch_textview=0x7f05004a;
+        public static final int next_switch_time_textview=0x7f05004b;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
         public static final int progress_circular=0x7f050018;
@@ -1029,13 +1045,14 @@ containing a value of this type.
         public static final int showTitle=0x7f050006;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
-        public static final int tab1=0x7f05003c;
-        public static final int tab2=0x7f05003e;
-        public static final int tab3=0x7f05003f;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003d;
+        public static final int temp_down_button=0x7f050045;
+        public static final int temp_layout=0x7f050042;
+        public static final int temp_textview=0x7f050043;
+        public static final int temp_up_button=0x7f050041;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
+        public static final int unit_temp_textview=0x7f050044;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1135,6 +1152,15 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int str_MoonIcon=0x7f0a0017;
+        public static final int str_SunIcon=0x7f0a0016;
+        public static final int str_celcius_unit=0x7f0a0010;
+        public static final int str_date=0x7f0a0012;
+        public static final int str_day=0x7f0a0013;
+        public static final int str_farenheit_unit=0x7f0a0011;
+        public static final int str_next_switch=0x7f0a0014;
+        public static final int str_nswitch_time=0x7f0a0015;
+        public static final int str_temperature=0x7f0a000f;
     }
     public static final class style {
         /** 
